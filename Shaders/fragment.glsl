f8dbd3fd6badd4d91abfa44b4346a3e7d5571f9a
Jamie <jamiehyland01@gmail.com>
2025-02-02 12:50:46 +0000

    misc changes

---
+++ Shaders/fragment.glsl
@@ -0,0 +1,13 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoord;
+
+// texture samplers
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main()
+{
+	FragColor = vec4(1.0,0.5,1.0,1.0);
+}
---
+++ src/headers/nuklear_container.h
@@ -0,0 +1,16 @@
+#ifndef nuklear_container_h
+#define nuklear_container_h
+#define MAX_VERTEX_BUFFER 512 * 1024
+#define MAX_ELEMENT_BUFFER 128 * 1024
+#define NK_INCLUDE_FIXED_TYPES
+#define NK_INCLUDE_STANDARD_IO
+#define NK_INCLUDE_STANDARD_VARARGS
+#define NK_INCLUDE_DEFAULT_ALLOCATOR
+#define NK_INCLUDE_VERTEX_BUFFER_OUTPUT
+#define NK_INCLUDE_FONT_BAKING
+#define NK_INCLUDE_DEFAULT_FONT
+#define NK_IMPLEMENTATION
+#define NK_SDL_GL3_IMPLEMENTATION
+#include "../../include/Nuklear/nuklear.h"
+#include "../../include/Nuklear/nuklear_sdl_gl3.h"
+#endif
