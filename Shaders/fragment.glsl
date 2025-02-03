1413dfccdb217dc476d3a8dbe6f0bc2fa9c2802a
Jamie <jamiehyland01@gmail.com>
2025-02-03 13:25:49 +0000

    misc

--- Shaders/fragment.glsl
+++ Shaders/fragment.glsl
@@ -2,12 +2,18 @@
 out vec4 FragColor;
 
 in vec2 TexCoord;
-
+in vec3 FragPos;
 // texture samplers
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main()
 {
-	FragColor = vec4(1.0,0.5,1.0,1.0);
+    vec3 color = vec3(
+        mod(FragPos.x, 1.0),  // Map X to R
+        mod(FragPos.y, 1.0),  // Map Y to G
+        mod(FragPos.z, 1.0)   // Map Z to B
+    );
+
+    FragColor = vec4(color, 1.0);
 }
