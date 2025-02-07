6ccfcf91f94fc3bed18df728f2bb4d6ece2793e5
Jamie <jamiehyland01@gmail.com>
2025-02-07 17:45:03 +0000

    shader changes

--- Shaders/fragment.glsl
+++ Shaders/fragment.glsl
@@ -1,21 +1,17 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 Normal;
-in vec3 FragPos;
-// texture samplers
-uniform sampler2D texture1;
-uniform sampler2D texture2;
+
+
 uniform vec3 lightColor;
 uniform vec3 lightPos;
 uniform vec3 viewPos;
 
 struct Material {
-    vec3 ambient;
-    vec3 diffuse;
-    vec3 specular;
+    sampler2D diffuse;
+    sampler2D specular;    
     float shininess;
-}; 
+};
 struct Light {
     vec3 position;
   
@@ -24,6 +20,10 @@ struct Light {
     vec3 specular;
 };
 
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoords;
+
 uniform Light light; 
   
 uniform Material material;
@@ -31,23 +31,20 @@ uniform Material material;
 void main()
 {   
 
-    vec3 ambient  = light.ambient * material.ambient;
+    // ambient
+    vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
   	
     // diffuse 
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse  = light.diffuse * (diff * material.diffuse);
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
-    vec3 specular = light.specular * (spec * material.specular);  
-
-    
-    
-    
+    vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
         
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
