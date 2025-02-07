9293b0c9df5d75a2b80a32fbcc9db3ad463a9a0f
Jamie <jamiehyland01@gmail.com>
2025-02-07 17:44:50 +0000

    removed boilder plate code

--- Main/Src/renderer.c
+++ Main/Src/renderer.c
@@ -9,6 +9,7 @@
 #include "../Headers/camera.h"
 #include "../Headers/renderer.h"
 #include "../Headers/material.h"
+#include "../Headers/model.h"
 #include "../Headers/nuklear_container.h"
 
 #include <stdio.h>
@@ -16,24 +17,8 @@
 #include <stdbool.h>
 
 
-Material cubeMaterial;
 
-mat4 cubeModel;
-mat4 cubeView;
-mat4 cubeProjection;
-mat4 quadModel;
-mat4 quadView;
-mat4 quadProjection;
-mat4 lightModel;;
-mat4 lightView;
-mat4 lightProjection;
-Shader cubeShader;
-Shader quadShader;
-Shader lightShader;
-Texture tex1;
-Texture tex2;
-
-#define FPS 30
+#define FPS 60
 #define FRAME_TARGET_TIME  (1000 / FPS)
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -45,70 +30,11 @@ SDL_GLContext context = NULL;
 float fps;
 int window_width = 640;
 int window_height = 480;
-vec3 p = {0};
 int previous_frame_time = 0;
 float delta_time = 0; 
-float cubeVertices[] = {
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
-
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f,  0.0f,  1.0f,
-
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
-
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
-
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-         0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
-        -0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
-
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-         0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
-        -0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
-    };
-float quadVertices[] = {
-    // positions          // colors           // texture coords
-     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
-};
-unsigned int quadIndices[] = {  
-0, 1, 3, // first triangle
-1, 2, 3  // second triangle
-};
-vec3 lightColor = {0};
-unsigned int cubeVAO,cubeVBO,quadVAO,quadVBO,quadEBO,lightVAO,lightEBO;
-vec3 lightPos = {0.0,2.0,5.0};
-
 float aspect_ratio;
+Model model;
 
-float a = 0;
 ///////////////////////////////////////////////////////////////////////////////
 // Setup function to initialize variables and game objects
 ///////////////////////////////////////////////////////////////////////////////
@@ -134,28 +60,14 @@ int setup_renderer(void) {
         fprintf(stderr, "Error creating SDL window");
         return false;
     }
-
     context = SDL_GL_CreateContext(window);
     SDL_GL_MakeCurrent(window, context);
     glewExperimental = GL_TRUE;
     glewInit();
     glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-
     initNuklear(window);
-    
-    glm_mat4_identity(cubeModel);
-    glm_mat4_identity(cubeView);
-    glm_mat4_identity(cubeProjection);
 
-    glm_mat4_identity(quadModel);
-    glm_mat4_identity(quadView);
-    glm_mat4_identity(quadProjection);
 
-    init_camera((vec3){0,1,35},(vec3){0,1,0});
-    setAmbient(&cubeMaterial,(vec3){1.0f, 0.5f, 0.31f});
-    setDiffuse(&cubeMaterial,(vec3){1.0f, 0.5f, 0.31f});
-    setSpecular(&cubeMaterial,(vec3){0.5f, 0.5f, 0.5f});
-    setShininess(&cubeMaterial, 32.0f);
 
     stbi_set_flip_vertically_on_load(1);
     glViewport(0, 0, window_width, window_height);
@@ -165,78 +77,14 @@ int setup_renderer(void) {
 // Initialize various aspects of OpenGL for rendering
 ///////////////////////////////////////////////////////////////////////////////
 bool init_opengl(void) {
-    glGenVertexArrays(1, &cubeVAO);
-    glGenBuffers(1, &cubeVBO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
-
-    glBindVertexArray(cubeVAO);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // normal attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-
-    glGenVertexArrays(1, &quadVAO);
-    glGenBuffers(1, &quadVBO);
-    glGenBuffers(1, &quadEBO);
-
-    glBindVertexArray(quadVAO);
-
-    glBindBuffer(GL_ARRAY_BUFFER, quadVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertices), quadVertices, GL_STATIC_DRAW);
-
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, quadEBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(quadIndices), quadIndices, GL_STATIC_DRAW);
-
-    // position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-    // color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
-    // texture coord attribute
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-    glEnableVertexAttribArray(2);
-
-    glGenVertexArrays(1,&lightVAO);
-    glBindVertexArray(lightVAO);
-    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-    glEnableVertexAttribArray(0);
-  
-
-    cubeShader.vertex_source    = "Shaders/vertex.glsl";
-    cubeShader.fragment_source  = "Shaders/fragment.glsl";
-
-    quadShader.vertex_source    = "Shaders/quadVertex.glsl";
-    quadShader.fragment_source  = "Shaders/quadFragment.glsl";
-
-    lightShader.vertex_source   = "Shaders/lightVertex.glsl";
-    lightShader.fragment_source = "Shaders/lightFragment.glsl";
-
-    init_shader(&cubeShader);
-    init_shader(&quadShader);
-    init_shader(&lightShader);
-
-    setShader(&cubeMaterial,&cubeShader);
-
-    tex1 = init_texture("Assets/Textures/container.jpg");
-    tex2 = init_texture("Assets/Textures/gear5.jpg");
-
-
-   return true;
+    //TODO: ADD initialization code for models
+    return true;
 }
 ///////////////////////////////////////////////////////////////////////////////
 // Update function frame by frame with a fixed time step
 ///////////////////////////////////////////////////////////////////////////////
 void update(void) {
     SDL_GetDesktopDisplayMode(0, &displayMode);
-    window_width = displayMode.w;
-    window_height = displayMode.h;
     // Frame timing
     int time_to_wait = FRAME_TARGET_TIME - (SDL_GetTicks() - previous_frame_time);
     if (time_to_wait > 0 && time_to_wait <= FRAME_TARGET_TIME) {
@@ -250,49 +98,26 @@ void update(void) {
 
     // Calculate FPS
     fps = (delta_time > 0) ? (1.0f / delta_time) : 0.0f;
-   
     process_keyboard_movement(delta_time);
 
-    
-    glm_mat4_identity(cubeModel);
-    //glm_translate(&cubeModel[0], (vec3){0.0,0.0,-5.0});
-    glm_rotate(&cubeModel[0], (a),(vec3){0.0,1.0,0.0});
-    aspect_ratio = (float)window_width / (float)window_height;
-    camera_look_at(&cubeView);
-    glm_perspective(glm_rad(fov), aspect_ratio, 0.1f, 100.0f, cubeProjection);
-
-    glm_mat4_identity(quadModel);
-    glm_translate(&quadModel[0], (vec3){-3.0,0.0,5.0});
-    //glm_rotate(&quadModel[0], (a),(vec3){0.0,1.0,0.0});
-    camera_look_at(&quadView);
-    aspect_ratio = (float)window_width / (float)window_height;
-    glm_perspective(glm_rad(fov), aspect_ratio, 0.1f, 100.0f, quadProjection);
-
-    glm_mat4_identity(lightModel);
-    glm_translate(&lightModel[0], lightPos);
-    glm_rotate(&lightModel[0], (a),(vec3){0.0,1.0,0.0});
-    camera_look_at(&lightView);
-    aspect_ratio = (float)window_width / (float)window_height;
-    glm_perspective(glm_rad(fov), aspect_ratio, 0.1f, 100.0f, lightProjection);
-    a+= 0.01;
-    
-    lightPos[2] = 20 * sin(a);
-    lightPos[0] = 20 * cos(-a);
-    
+   
 }
 ///////////////////////////////////////////////////////////////////////////////
 // Render function to draw user interface
 ///////////////////////////////////////////////////////////////////////////////
 void renderUI(){
-    static struct nk_colorf color = {1.0f, 0.0f, 0.0f, 1.0f};
+    static struct nk_colorf color = {1.0f, 1.0f, 1.0f, 1.0f};
     
     if (nk_begin(ctx, "Debug Window", nk_rect(0, 0, 410, 500), NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_MINIMIZABLE)) {
         
+        nk_layout_row_dynamic(ctx, 30, 2);
+        nk_label(ctx,"Frame Rate: ",NK_TEXT_ALIGN_LEFT);
+        nk_labelf(ctx, NK_TEXT_LEFT, "(%.2f)", fps);
         // Define row layout with two columns: label + value
         nk_layout_row_dynamic(ctx, 130, 2);
         // Display "Camera Position: "
         nk_label(ctx, "Camera Position:", NK_TEXT_LEFT);
-        
+        Vec3 p = {0};
         get_camera_position(&p);
         nk_labelf(ctx, NK_TEXT_LEFT, "(%.2f, %.2f, %.2f)", p[0], p[1], p[2]);
 
@@ -300,14 +125,7 @@ void renderUI(){
         nk_label(ctx, "Light Colour:", NK_TEXT_LEFT);
         color = nk_color_picker(ctx, color, NK_RGBA); // RGB mode (or NK_RGBA for alpha)
         
-        nk_layout_row_dynamic(ctx, 160, 2);
-        nk_label(ctx,"Frame Rate: ",NK_TEXT_ALIGN_LEFT);
-        nk_labelf(ctx, NK_TEXT_LEFT, "(%.2f)", fps);
-
-
-        lightColor[0] = color.r;
-        lightColor[1] = color.g;
-        lightColor[2] = color.b;
+    
     }
     nk_end(ctx);
 }
@@ -319,58 +137,13 @@ void render(void) {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     renderUI();
-
     glEnable(GL_DEPTH_TEST);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, tex1.id);
-    
-    glActiveTexture(GL_TEXTURE1); 
-    glBindTexture(GL_TEXTURE_2D, tex2.id);
-    glUseProgram(cubeMaterial.shader->shaderID);
-
-    set_matrix(cubeShader.shaderID,"model",cubeModel);
-    set_matrix(cubeShader.shaderID,"view",cubeView);
-    set_matrix(cubeShader.shaderID,"projection",cubeProjection);
-    set_vec3(cubeShader.shaderID,"lightColor", lightColor);
-    set_vec3(cubeShader.shaderID,"lightPos", lightPos);
-    set_vec3(cubeShader.shaderID,"viewPos", p);
-    set_vec3(cubeMaterial.shader->shaderID,"material.ambient",cubeMaterial.ambient);
-    set_vec3(cubeMaterial.shader->shaderID,"material.diffuse",cubeMaterial.diffuse);
-    set_vec3(cubeMaterial.shader->shaderID,"material.specular",cubeMaterial.specular);
-    set_float(cubeMaterial.shader->shaderID,"material.shininess",cubeMaterial.shininess);
-    set_vec3(cubeMaterial.shader->shaderID, "light.ambient",  (vec3){0.2f, 0.2f, 0.2f});
-    set_vec3(cubeMaterial.shader->shaderID, "light.diffuse",  (vec3){0.5f, 0.5f, 0.5f});
-    set_vec3(cubeMaterial.shader->shaderID, "light.specular",  (vec3){1.0f, 1.0f, 1.0f});
-
-    for(int i = -5; i < 5; i ++ ){
-        for(int j = -5; j < 5; j ++ ){
-            glm_mat4_identity(cubeModel);
-            glm_translate(&cubeModel[0], (vec3){1.5 * i,-1.0,-1.5 * j});
-            set_matrix(cubeShader.shaderID,"model",cubeModel);
-            glBindVertexArray(cubeVAO);
-            glDrawArrays(GL_TRIANGLES, 0, 36);
-        }
-    }
-
-    glUseProgram(lightShader.shaderID);
-    set_matrix(lightShader.shaderID,"model",lightModel);
-    set_matrix(lightShader.shaderID,"view",lightView);
-    set_matrix(lightShader.shaderID,"projection",lightProjection);
-    set_vec3(lightShader.shaderID,"lightColor", lightColor);
-    glBindVertexArray(lightVAO);
-    glDrawArrays(GL_TRIANGLES, 0, 36);
 
 
-    // glUseProgram(quadShader.shaderID);
-    // set_matrix(quadShader.shaderID,"model",quadModel);
-    // set_matrix(quadShader.shaderID,"view",quadView);
-    // set_matrix(quadShader.shaderID,"projection",quadProjection);
-    // set_int(quadShader.shaderID,"texture1",0);
-    // set_int(quadShader.shaderID,"texture2",1);
+
     
 
-    // glBindVertexArray(quadVAO);
-    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
 
 
     
